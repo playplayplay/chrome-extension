8829782db550a92f3b8bf4bb355fb50a451f8cdc
Stefan Buck <github@stefanbuck.com>
2015-09-25 00:17:52 +0200

    Reinitialize project

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'browserify', 'fixture'],
+    files: [
+      'test/fixtures/*.html',
+      'test/**/*.spec.js'
+    ],
+    exclude: [],
+    preprocessors: {
+      'test/fixtures/*.html': ['html2js'],
+      'app/src/**/*.js': ['browserify'],
+      'test/**/*.spec.js': ['browserify']
+    },
+    browserify: {
+     debug: true,
+     transform: [
+       ['babelify']
+      ]
+    },
+    babelPreprocessor: {},
+    reporters: ['progress'],
+    browsers: ['PhantomJS'],
+    singleRun: true
+  });
+};
